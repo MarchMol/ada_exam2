4975d05fa80843e0cf4ba55e94c9e616353a2ad8
MarchMol <marchmol98@gmail.com>
2025-04-04 21:46:54 -0600

    fix: top-down working correctly and commented

--- prob_1.py
+++ prob_1.py
@@ -1,34 +1,41 @@
 
 def nokia_teclas_aux(a, n, memo,d):
-    if memo[n-1][d-1] != 0:
-        return memo[n-1][d-1]
+    # Si existe memoizacion, usar esa
+    if memo[n-1][d] != 0:
+        return memo[n-1][d]
+    # si es caso base, solo obtener cuantos adyacentes hay
     if n == 1:
-        q = 0
+        q = len(a[d])
+    # Recursion
     else:
-        q= -10000
-        for i in range(1,n):
-            tem = 0
-            for j in range(0,10):
-                tem+= nokia_teclas_aux(a,i-1,memo,j)
-            q = max(
-                q,
-                len(a[i-1]) + tem
-            )
-        memo[n-1][d-1] = q
+        q = 0
+        # Para todas las adyacencias, del digito actual, recursion
+        for j in a[d]:
+            q+=nokia_teclas_aux(a,n-1,memo,j)
+        # Guardar en memoizacion (no hay que maximizar, solo guardar)
+        memo[n-1][d] = q
     return q
 
 def nokia_teclas(a,n):
-    memo = [[0]*10]*n
-    val = 0
-    for i in range(0,10): # 0-9
-        val+= nokia_teclas_aux(a,n,memo,i)
-    return val
+    # Inicializacion de memoizacion
+    memo = []
+    for i in range(0,n):
+        tem = []
+        for i in range(0,10):
+            tem.append(0)
+        memo.append(tem)
+    # suma de 
+    rslt_ = 0
+    for i in range(0,10): # para todos los botones 0-9
+        rslt_+= nokia_teclas_aux(a,n,memo,i)
+    return rslt_
    
 # 1 2 3
 # 4 5 6
 # 7 8 9
 #   0
-#      0  1  2  3  4  5  6  7  8  9
+
+# adyacencia
 a_ = {
     0: [0,8],
     1: [1,2,4],
@@ -41,7 +48,9 @@ a_ = {
     8: [5,7,8,9,0],
     9: [6,8,9]
 }
-n_ = 2
+# n
+n_ = 1
+# resultado
 rslt = nokia_teclas(a_,n_)
 print(rslt)
 
